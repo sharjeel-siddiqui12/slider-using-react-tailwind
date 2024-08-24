d7f8bef324142cd9a341b98183cc49687f85b128
sharjeel-siddiqui12 <sharjeelsiddiqui00@gmail.com>
2024-08-25 00:59:21 +0500

    Update Slider component to include max-sd margin for description

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import React, { useState, useEffect, useRef } from 'react';
 import Slider from './components/Slider.jsx';
 import Home from './components/Home.jsx';   
 
-
-
-
 const App = () => {
     return (
         <div>
